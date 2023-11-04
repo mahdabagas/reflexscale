e687bd00903a4b99134ec6f3bb3159a688eaf0ad
Bagas Mahda Dhani <muksinsaja12@gmail.com>
2023-11-05 02:50:53 +0700

    add: tailwind and configure

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      black: "#1E1E1E",
+      primary: "#F976AC",
+      secondary: "#FEE0E0",
+      blue: "#0099FF",
+      white: "#FFF7F5"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
