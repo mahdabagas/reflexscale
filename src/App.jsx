bac1cd0de4548065100cee53030e61b7513db5c9
Bagas Mahda Dhani <muksinsaja12@gmail.com>
2023-11-08 20:27:45 +0700

    edit: configure lazy loading and structure route

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,52 @@
 import { Outlet, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Register from "./pages/Register";
-import Test from "./pages/Test";
-import Profile from "./pages/Profile";
-import ResultTest from "./pages/ResultTest";
+import PrivateRoute from "./components/routes/PrivateRoute";
+import ProfileCtx from "./context/ProfileContext";
+import LoginCtx from "./context/LoginContext";
+import { Suspense, lazy } from "react";
+import PageLoading from "./components/loader/PageLoading";
+
+const Home = lazy(() => wait(1000).then(() => import("../src/pages/Home")));
+const Test = lazy(() => wait(1000).then(() => import("../src/pages/Test")));
+const Profile = lazy(() =>
+  wait(1000).then(() => import("../src/pages/Profile"))
+);
+const Register = lazy(() =>
+  wait(1000).then(() => import("../src/pages/Register"))
+);
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/test" element={<Test />} />
-        <Route path="/test/result" element={<ResultTest />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+      <LoginCtx>
+        <ProfileCtx>
+          <Routes>
+            <Route element={<SuspenseWrapper />}>
+              <Route path="/" element={<Home />} />
+              <Route element={<PrivateRoute />}>
+                <Route path="/test" element={<Test />} />
+                <Route path="/profile" element={<Profile />} />
+              </Route>
+              <Route path="/register" element={<Register />} />
+            </Route>
+          </Routes>
+        </ProfileCtx>
+      </LoginCtx>
     </div>
   );
 }
+
+const SuspenseWrapper = () => {
+  return (
+    <Suspense fallback={<PageLoading />}>
+      <Outlet />
+    </Suspense>
+  );
+};
+
+const wait = (time) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, time);
+  });
+};
 
 export default App;
