e4fd5b6df7303cc736f11b12830fa897fae7cbf3
Bagas Mahda Dhani <muksinsaja12@gmail.com>
2023-11-08 20:23:06 +0700

    add: props disabled

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,8 +1,9 @@
-const Button = ({ children, type, onClick, className, loading }) => {
+const Button = ({ children, type, onClick, className, loading, disabled }) => {
   return (
     <button
       type={type}
       onClick={onClick}
+      disabled={disabled}
       className={`p-2 flex items-center justify-center transition-colors duration-150 ${className}`}
     >
       {loading ? "Loading..." : children}
