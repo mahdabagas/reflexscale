6fd010f103eee1344f0b59cb74db6b50044cabbc
Bagas Mahda Dhani <muksinsaja12@gmail.com>
2023-11-08 20:23:37 +0700

    edit: style input

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -7,18 +7,20 @@ const Input = ({
   name,
   classname,
   defaultValue,
+  id,
 }) => {
   return (
     <>
       <input
         name={name}
         type={type}
+        id={id}
         onChange={onChange}
         placeholder={placeholder}
         disabled={disabled}
         required={required}
         defaultValue={defaultValue}
-        className={`rounded-lg p-2 drop-shadow-md ${classname}`}
+        className={`rounded-lg p-2 drop-shadow-md bg-white w-full focus:outline-primary ${classname}`}
       />
     </>
   );
