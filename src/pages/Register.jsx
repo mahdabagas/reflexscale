ddd5910705c2b6a3a3bc4e2be15dfd7117cb349e
Bagas Mahda Dhani <muksinsaja12@gmail.com>
2023-11-08 20:22:11 +0700

    edit: responsive and animation

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -5,17 +5,19 @@ import RegisterForm from "../feature/auth/components/RegisterForm";
 function Register() {
   return (
     <>
-      <main className="min-h-screen flex justify-center gap-48 items-center px-8 lg:px-10 relative ">
-        <div className="hidden lg:flex flex-col gap-16 text-white items-center relative z-10  ">
-          <h1 className="text-6xl font-medium ">REFLEXSCALE</h1>
-          <img src={Logo} alt="Logo" className="w-64 h-64" />
-          <h2 className="text-3xl">Risk Level of Free Sex Scale</h2>
+      <main className="min-h-screen flex justify-center gap-48 items-center px-8 lg:px-10 relative bg-white ">
+        <div className="animate-fade-right hidden lg:flex flex-col gap-16 text-white items-center relative z-10  ">
+          <h1 className="text-6xl font-medium drop-shadow-lg">REFLEXSCALE</h1>
+          <img src={Logo} alt="Logo" className="w-64 h-64 drop-shadow-lg" />
+          <h2 className="text-3xl drop-shadow-lg">
+            Risk Level of Free Sex Scale
+          </h2>
         </div>
-        <div className="relative z-10">
+        <div className="relative z-10 py-2 animate-fade animate-delay-500">
           <RegisterForm />
         </div>
         {/* Background */}
-        <div className="w-full h-full absolute top-0 bottom-0 -left-20 -z-0 ">
+        <div className="animate-fade-right w-full h-full absolute top-0 bottom-0 -left-20 -z-0 ">
           <img
             src={bgHorizontal}
             alt="background-vertical"
